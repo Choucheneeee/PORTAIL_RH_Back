966a6aca8ef69d43d5d4d86e68aff47f888383d2
Choucheneeee <chouchene.amine.etud@gmail.com>
2025-02-13 22:08:48 +0100

    change the response of update user api

--- controllers/userController.js
+++ controllers/userController.js
@@ -23,6 +23,16 @@ const updateuser = async (req, res) => {
     const userId = req.user.id;  // Ensure that `authMiddleware` sets `req.user`
     console.log(req.body);  // Logs the update data
 
+    // Convert the date fields in req.body to Date objects if they exist and are in the correct format
+    if (req.body.personalInfo && req.body.personalInfo.birthDate) {
+      // Convert 'dd/mm/yyyy' to 'yyyy-mm-dd' format before converting to a Date object
+      req.body.personalInfo.birthDate = new Date(req.body.personalInfo.birthDate.split('/').reverse().join('-'));
+    }
+    if (req.body.professionalInfo && req.body.professionalInfo.hiringDate) {
+      // Convert 'dd/mm/yyyy' to 'yyyy-mm-dd' format before converting to a Date object
+      req.body.professionalInfo.hiringDate = new Date(req.body.professionalInfo.hiringDate.split('/').reverse().join('-'));
+    }
+
     // Find the user by ID and update the fields
     const updatedUser = await User.findByIdAndUpdate(userId, req.body, { new: true });
 
@@ -30,12 +40,13 @@ const updateuser = async (req, res) => {
       return res.status(404).send('User not found');
     }
 
-    // Return the updated user
-    res.json(updatedUser);
+    res.json("User updated successfully");
   } catch (error) {
     console.error(error);
     res.status(500).send('Error updating user');
   }
 };
+
+
 
 exports.updateuser = updateuser;
