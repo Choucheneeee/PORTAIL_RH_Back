d26895b52fab52b3ce6ad042ee80dbc02170a2a3
Choucheneeee <chouchene.amine.etud@gmail.com>
2025-03-25 17:15:58 +0100

    delete empty route

--- server.js
+++ server.js
@@ -166,7 +166,6 @@ app.use(cors("*"));
 app.use(express.json());
 
 // Routes
-app.get("/", (req, res) => res.json("Connected!"));
 app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/user", require("./routes/userRoutes"));
 app.use("/api/requests", require("./routes/requestRoutes"));
